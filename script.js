6402fe0eca60ed12644b76d6ab23fafa47a88c01
Tom Bijesse <tom@bijesse.com>
2016-06-09 11:16:07 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(library.music.eighties.Michael_Jackson[0].year);
+console.log(library.music.nineties.Hanson[0].year);
+console.log(library.music.two_thousands.Lady_Gaga[0].year);
+console.log(library.music.twenty_tens.Adele[0].song);
