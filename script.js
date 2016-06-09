e784cbe4ea9800e5912a9cbaa9187ca17c0c5b07
Tom Bijesse <bijesse.thomas@gmail.com>
2016-06-09 15:34:27 +0000

    added script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+//Task 1: Console the year in which the song "Beat it" was relased 
 console.log(library.music.eighties.Michael_Jackson[0].year);
-console.log(library.music.nineties.Hanson[0].year);
-console.log(library.music.two_thousands.Lady_Gaga[0].year);
-console.log(library.music.twenty_tens.Adele[0].song);
+
+//Task 2: Console the name of the song Lady Gaga released in 2006 
+console.log(library.music.two_thousands.Lady_Gaga[0].song);
+
+
+$( document ).ready(function() {
+//Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
+    
+/*Task 4: Create a site that can search display songs from an artist specified by the user.
+        Step 1: Create an input field and search button on the HTML page. The input fiels must ask the user to type in an Artist's name
+        Step 2: Using the value of what the user has typed in the input field display the names of all songs by that artist.
+*/    
+    
+});
